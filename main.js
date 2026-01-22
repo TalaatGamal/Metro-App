9e323a5866f5eb3532e2e576144fd00cfa2649d1
TalaatGamal <talaatgamal.dev@gmail.com>
2026-01-22 16:45:36 +0200

    lio

--- main.js
+++ main.js
@@ -14,6 +14,76 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
+
+
+
+let lines_p = document.getElementById("lines-page")
+let tickets_p = document.getElementById("tickets-page")
+let subscribe_p = document.getElementById("subscribe-page")
+
+let lines_i = document.getElementById("lines-icon")
+let tickets_i = document.getElementById("tickets-icon")
+let subscribe_i = document.getElementById("subscribe-icon")
+
+
+
+// *****************************************************************
+lines_i.addEventListener("click", function () {
+    lines_p.style.display="flex"
+    setTimeout(() => {
+        lines_p.style.opacity="1"
+        lines_p.style.width="100vw"
+        // lines_p.style.height="100vh"
+    }, 10);
+})
+// *****************************************************************
+tickets_i.addEventListener("click", function () {
+    tickets_p.style.display="flex"
+    setTimeout(() => {
+        tickets_p.style.opacity="1"
+        tickets_p.style.width="100vw"
+        // tickets_p.style.height="100vh"
+    }, 10);
+})
+// *****************************************************************
+subscribe_i.addEventListener("click", function () {
+    subscribe_p.style.display="flex"
+    setTimeout(() => {
+        subscribe_p.style.opacity="1"
+        subscribe_p.style.width="100vw"
+        // subscribe_p.style.height="100vh"
+    }, 10);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mobile app mode
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./sw.js")
     .then(() => console.log("SW registered"))
@@ -28,3 +98,5 @@ map.addEventListener("click", function () {
         // "_blank"
     );
 })
+
+// mobile app mode
